ced4240a1dddad39bff4626e9624f816d9677225
EgERohit2 <118536484+EgERohit2@users.noreply.github.com>
2023-03-11 18:26:41 +0530

    Add files via upload

---
+++ NumbersPattern.java
@@ -0,0 +1,22 @@
+package Pattern;
+
+public class NumbersPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 5;
+		int count = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (count < 10) {
+					System.out.print("0");
+				}
+				System.out.print(count + " ");
+				count++;
+			}
+			System.out.println();
+		}
+	}
+
+}
